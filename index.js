81ff2f51e55d99c9cd52582bcd8d66b55e893811
belanovic <goranbelanovic@yahoo.com>
2024-07-30 00:27:24 +0200

    msg

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ process.env.TZ = "Europe/Belgrade";
 const hostIP = config.get('hostIP');
 const port = process.env.PORT || 4000; 
 
-console.log('evo meeeeeeeeeeeeeeeeeeeeeeeee')
+console.log('evo meee')
 
 server.listen(port, hostIP, () => console.log(`Server is listening on port ${port}`));
 
